3ec37b26cbc97c5b721285a88e0b3debbec68a5a
JeshwanthAshadapu <63444708+JeshwanthAshadapu@users.noreply.github.com>
2021-02-21 12:26:29 +0530

    Create Binarytreenode.java

---
+++ Trees/Binarytreenode.java
@@ -0,0 +1,14 @@
+public class BTNode{
+    int data;
+    BTNode left;
+    BTNode right;
+    BTNode(int data){
+        this.data=data;
+        this.left=null;
+        this.right=null;
+    }
+    BTNode(){
+        this.data=data;
+        this.left=null;
+        this.right=null;
+    }
